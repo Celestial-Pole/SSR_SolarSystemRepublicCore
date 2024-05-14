56d17aaecc69c9c7f67d508133ccec51cba09835
FelixTaXk <974371843@qq.com>
2024-05-14 08:51:35 +0800

    Base

---
+++ UpdateDLL.bat
@@ -0,0 +1,6 @@
+DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\FS_SSR.dll"
+COPY "C:\Users\Fleurety\Desktop\rw\DebugTweak\FS_SSR\bin\Debug\FS_SSR.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\"
+
+DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\FS_SSR_Combat.dll"
+COPY "C:\Users\Fleurety\Desktop\rw\DebugTweak\FS_SSR_Combat\bin\Debug\FS_SSR_Combat.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\"
+PAUSE
