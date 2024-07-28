c2ac4819ab49019745a3955967848c51c7fe63e3
Future <1609605489@qq.com>
2024-07-28 09:31:26 +0800

    fix bug

--- src/SSR.Effect/Graphic_Perfab.cs
+++ src/SSR.Effect/Graphic_Perfab.cs
@@ -9,9 +9,9 @@ namespace SSR.Effect
         public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, float extraRotation)
         {
             CompUnityGameObject compUnityGameObject = thing.TryGetComp<CompUnityGameObject>();
-            loc += DrawOffset(rot);
             if(compUnityGameObject != null)
             {
+                loc += DrawOffset(rot);
                 if(compUnityGameObject.MaskTransform)
                 {
                     compUnityGameObject.MaskTransform.localPosition = new Vector3(0,loc.y,0);
@@ -22,7 +22,7 @@ namespace SSR.Effect
                     compUnityGameObject.Transform.position = loc;
                 }
                 compUnityGameObject.Transform.localScale = new Vector3(drawSize.x,1.0f,drawSize.y);
-                compUnityGameObject.Transform.rotation = Quaternion.Euler(0,extraRotation,0);
+                compUnityGameObject.Transform.rotation = Quaternion.Euler(0,extraRotation + rot.AsAngle,0);
                 compUnityGameObject.SetVisibility(true);
             }
             else base.DrawWorker(loc, rot, thingDef, thing, extraRotation);
