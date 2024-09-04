718fe14904e7f38b19251154f59d7fd38915c59d
MoonShine <ljhmyw@foxmail.com>
2024-09-04 22:41:09 +0800

    minorfix

--- UpdateDLL.bat
+++ UpdateDLL.bat
@@ -3,4 +3,7 @@ COPY "C:\Users\Fleurety\Desktop\rw\SSR_Framework\Solar-System-Republic-Framework
 
 DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\FS_SSR_Combat.dll"
 COPY "C:\Users\Fleurety\Desktop\rw\\SSR_Framework\Solar-System-Republic-Framework\FS_SSR_Combat\bin\Debug\FS_SSR_Combat.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\"
+
+DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\FS_SSR_Compatibility.dll"
+COPY "C:\Users\Fleurety\Desktop\rw\\SSR_Framework\Solar-System-Republic-Framework\FS_SSR_Compatibility\bin\Debug\FS_SSR_Compatibility.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\"
 PAUSE
