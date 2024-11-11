a855b043bec93b2e7642ce71b8ca1a6848946998
MoonShine <ljhmyw@foxmail.com>
2024-11-11 20:58:47 +0800

    修复了火力发电机不填燃料

--- UpdateDLL.bat
+++ UpdateDLL.bat
@@ -6,7 +6,7 @@ DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemb
 COPY "C:\Users\Fleurety\Desktop\rw\SSR_Framework\Solar-System-Republic-Framework\FS_SSR\bin\Debug\FS_SSR.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\"
 
 DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\FS_SSR_Combat.dll"
-COPY "C:\Users\Fleurety\Desktop\rw\\SSR_Framework\Solar-System-Republic-Framework\FS_SSR_Combat\bin\Debug\FS_SSR_Combat.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\"
+COPY D:\Repository\SSR_Combat\bin\Debug\FS_SSR_Combat.dll "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\"
 
 DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\FS_SSR_Compatibility.dll"
 COPY "C:\Users\Fleurety\Desktop\rw\\SSR_Framework\Solar-System-Republic-Framework\FS_SSR_Compatibility\bin\Debug\FS_SSR_Compatibility.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\"
