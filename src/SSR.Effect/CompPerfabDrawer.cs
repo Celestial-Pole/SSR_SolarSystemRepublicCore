77da863007509f88e83f7923c6ad2e0d20c78a2c
Future <1609605489@qq.com>
2025-01-19 20:56:53 +0800

    update multi view mode

--- src/SSR.Effect/CompPerfabDrawer.cs
+++ src/SSR.Effect/CompPerfabDrawer.cs
@@ -9,19 +9,22 @@ namespace SSR.Effect
 {
     public class CompPerfabDrawer : ThingComp
     {
-        internal class UnityGameObjectUpdater : MonoBehaviour
+        //custom drawer componet for unity perfab opration
+        public class UnityGameObjectUpdater : MonoBehaviour
         {
-            public bool showInNextFrame = true;
-            public bool isPlaying = false;
-            public long expirationTick = 6000;
-            public float playingSpeed = 1;
-            public Transform maskTransform;
-            public Transform ownTransform;
-            public GameObject ownGameObject;
-            public Animator ownAnimator;
-            public CompPerfabDrawer compUnityGameObject;
+            public uint level; //current level of perfab
+            public Rot4 rot; //current rotation of perfab
+            public bool showInNextFrame = true; //show in next frame, avoid the object showing when graphic object not invoke draw function
+            public bool isPlaying = false; //animation playing
+            public long expirationTick = 6000; //delet condataion, when this object not update by `Graphic_Perfab_Single` or `Graphic_Perfab_Multi` for 10 seconds
+            public float playingSpeed = 1; //animation speed
+            public Transform maskTransform; //cached mask transform
+            public Transform ownTransform; //cached behaviour ownner transform
+            public GameObject ownGameObject; //cached behaviour ownner gameobject
+            public Animator ownAnimator; //cached behaviour ownner animator
+            public CompPerfabDrawer compUnityGameObject; //controler of this gameobject
 
-            public void SetVisibility(bool visibility)
+            private void SetVisibility(bool visibility)
             {
                 foreach(var i in ownTransform.GetComponentsInChildren<MeshRenderer>())
                 {
@@ -34,10 +37,12 @@ namespace SSR.Effect
                 ownGameObject = ownGameObject ?? base.gameObject;
                 if(compUnityGameObject != null)
                 {
+                    //standard animation parameters, please ensure the animator use thoes parameters
                     ownAnimator?.SetFloat("Speed", Find.TickManager.TickRateMultiplier * (Find.TickManager.Paused ? 0 : 1) * playingSpeed);
                     ownAnimator?.SetBool("IsPlaying", isPlaying);
                     SetVisibility(showInNextFrame);
                     showInNextFrame = false;
+                    //delet condataion, when this object not update by `Graphic_Perfab_Single` or `Graphic_Perfab_Multi` for 10 seconds
                     if(expirationTick <= 0)
                     {
                         GameObject.Destroy(ownGameObject);
@@ -51,38 +56,14 @@ namespace SSR.Effect
                 }
             }
         }
-        private int currentIndex = 0;
+        private uint currentIndex = 0;
         private UnityGameObjectUpdater monoBehaviour;
         
-        
-        public CompProperties_PerfabDrawer Props => (CompProperties_PerfabDrawer)props;
-
-
-        public bool IsPlaying
+        public uint PerfabLevel
         {
-            get => UnityGameObjectUpdaterComponet?.isPlaying ?? false;
+            get => currentIndex;
             set
             {
-                if(UnityGameObjectUpdaterComponet != null)
-                    UnityGameObjectUpdaterComponet.isPlaying = value;
-            }
-        }
-        public float PlaySpeed
-        {
-            get => UnityGameObjectUpdaterComponet?.playingSpeed ?? 0;
-            set
-            {
-                if(UnityGameObjectUpdaterComponet != null)
-                    UnityGameObjectUpdaterComponet.playingSpeed = value;
-            }
-        }
-
-        public int PerfabLevel
-        {
-            get => Math.Max(Math.Min(currentIndex, Props.GameObjects.Count - 1),0);
-            set
-            {
-                value = Math.Min(value, Props.GameObjects.Count - 1);
                 if (currentIndex != value && value >= 0)
                 {
                     currentIndex = value;
@@ -92,60 +73,82 @@ namespace SSR.Effect
             }
         }
 
-
-
-        private UnityGameObjectUpdater UnityGameObjectUpdaterComponet
-        {
-            get
-            {
-                if(monoBehaviour == null)
-                {
-                    // Log.Message(ShaderDatabase.Cutout.renderQueue);
-                    if (Props.GameObjects.Count > 0)
-                    {
-                        GameObject obj = GameObject.Instantiate(Props.GameObjects[PerfabLevel]);
-                        monoBehaviour = obj.AddComponent<UnityGameObjectUpdater>();
-                        monoBehaviour.ownGameObject = obj;
-                        monoBehaviour.ownTransform = obj.transform;
-                        monoBehaviour.maskTransform = monoBehaviour.ownTransform.Find("FinalMask");
-                        monoBehaviour.ownAnimator = obj.GetComponent<Animator>();
-                        monoBehaviour.compUnityGameObject = this;
-                    }
-                }
-                return monoBehaviour;
-            }
-        }
-
-        public GameObject GameObject => UnityGameObjectUpdaterComponet?.ownGameObject;
-
-        public Transform Transform => UnityGameObjectUpdaterComponet?.ownTransform;
-
-        public Transform MaskTransform => UnityGameObjectUpdaterComponet?.maskTransform;
         
-        public Animator Animator => UnityGameObjectUpdaterComponet?.ownAnimator;
+        public CompProperties_PerfabDrawer Props => props as CompProperties_PerfabDrawer;
 
-        public void ShowInNextFrame()
+        public UnityGameObjectUpdater UnityGameObjectUpdaterComponet(Rot4 rot)
         {
-            UnityGameObjectUpdaterComponet.showInNextFrame = true;
-            UnityGameObjectUpdaterComponet.expirationTick = 6000;
+            if(monoBehaviour == null || monoBehaviour.level != PerfabLevel || monoBehaviour.rot != rot)
+            {
+                // Log.Message(ShaderDatabase.Cutout.renderQueue);
+                GameObject perfab = Props.GetGameObjects(PerfabLevel, rot);
+                if (perfab != null)
+                {
+                    GameObject obj = GameObject.Instantiate(perfab);
+                    monoBehaviour = obj.AddComponent<UnityGameObjectUpdater>();
+                    monoBehaviour.ownGameObject = obj;
+                    monoBehaviour.ownTransform = obj.transform;
+                    monoBehaviour.maskTransform = monoBehaviour.ownTransform.Find("FinalMask");
+                    monoBehaviour.ownAnimator = obj.GetComponent<Animator>();
+                    monoBehaviour.compUnityGameObject = this;
+                }
+            }
+            return monoBehaviour;
         }
 
     }
 
-    public class CompProperties_PerfabDrawer : CompProperties
+    public abstract class CompProperties_PerfabDrawer : CompProperties
     {
-        
-        public List<string> leveledPerfabPaths;
-        internal List<GameObject> leveledPerfabs;
         public CompProperties_PerfabDrawer()
         {
             compClass = typeof(CompPerfabDrawer);
         }
 
-        internal List<GameObject> GameObjects
+        internal abstract GameObject GetGameObjects(uint level, Rot4 rot);
+
+    }
+
+    //single view perfab drawer
+    public class CompProperties_SinglePerfabDrawer : CompProperties_PerfabDrawer
+    {
+        
+        //do not use this class in xml def file, it will setting automatically when you use this comp
+        private class Graphic_Perfab_Single : Graphic_Single
         {
-            get
+            public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, float extraRotation)
             {
+                CompPerfabDrawer.UnityGameObjectUpdater obj = thing.TryGetComp<CompPerfabDrawer>()?.UnityGameObjectUpdaterComponet(rot);
+                if(obj)
+                {
+                    loc += DrawOffset(rot);
+                    if(obj.maskTransform)
+                    {
+                        obj.maskTransform.localPosition = new Vector3(0,loc.y,0);
+                        obj.ownTransform.position = new Vector3(loc.x,0,loc.z);
+                    }
+                    else
+                    {
+                        obj.ownTransform.position = loc;
+                    }
+                    obj.ownTransform.localScale = new Vector3(drawSize.x,1.0f,drawSize.y);
+                    obj.ownTransform.rotation = Quaternion.Euler(0,extraRotation + rot.AsAngle,0);
+                    obj.expirationTick = 6000;
+                    obj.showInNextFrame = true;
+                }
+                else base.DrawWorker(loc, rot, thingDef, thing, extraRotation);
+            }
+
+            public override Graphic GetColoredVersion(Shader newShader, Color newColor, Color newColorTwo)
+            {
+                return this;
+            }
+        }
+        public List<string> leveledPerfabPaths;
+        private List<GameObject> leveledPerfabs;
+
+        internal override GameObject GetGameObjects(uint level, Rot4 rot)
+        {
                 if(leveledPerfabs == null)
                 {
                     leveledPerfabs = new List<GameObject>();
@@ -158,24 +161,127 @@ namespace SSR.Effect
                         }
                     }
                 }
-                return leveledPerfabs;
-            }
+                return leveledPerfabs[(int)level];
         }
 
         public override void ResolveReferences(ThingDef parentDef)
         {
-            parentDef.graphicData.graphicClass = typeof(Graphic_Perfab);
+            parentDef.graphicData.graphicClass = typeof(Graphic_Perfab_Single);
             parentDef.graphic = null;
             parentDef.drawerType = DrawerType.RealtimeOnly;
         }
 
-        ~CompProperties_PerfabDrawer()
+        ~CompProperties_SinglePerfabDrawer()
         {
             foreach (GameObject gameObject in leveledPerfabs)
             {
                 UnityEngine.Object.Destroy(gameObject);
             }
         }
+    }
 
+    //multi view perfab drawer
+    public class CompProperties_MultiPerfabDrawer : CompProperties_PerfabDrawer
+    {
+        public struct PerfabPathInfo
+        {
+            public string north;
+            public string south;
+            public string west;
+            public string east;
+        }
+        public struct PerfabInfo
+        {
+            public GameObject north;
+            public GameObject south;
+            public GameObject west;
+            public GameObject east;
+        }
+        //do not use this class in xml def file, it will setting automatically when you use this comp
+        private class Graphic_Perfab_Multi : Graphic_Multi
+        {
+            public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, float extraRotation)
+            {
+                CompPerfabDrawer.UnityGameObjectUpdater obj = thing.TryGetComp<CompPerfabDrawer>()?.UnityGameObjectUpdaterComponet(rot);
+                if(obj)
+                {
+                    loc += DrawOffset(rot);
+                    if(obj.maskTransform)
+                    {
+                        obj.maskTransform.localPosition = new Vector3(0,loc.y,0);
+                        obj.ownTransform.position = new Vector3(loc.x,0,loc.z);
+                    }
+                    else
+                    {
+                        obj.ownTransform.position = loc;
+                    }
+                    obj.ownTransform.localScale = new Vector3(drawSize.x,1.0f,drawSize.y);
+                    obj.ownTransform.rotation = Quaternion.Euler(0,extraRotation + rot.AsAngle,0);
+                    obj.expirationTick = 6000;
+                    obj.showInNextFrame = true;
+                }
+                else base.DrawWorker(loc, rot, thingDef, thing, extraRotation);
+            }
+
+            public override Graphic GetColoredVersion(Shader newShader, Color newColor, Color newColorTwo)
+            {
+                return this;
+            }
+        }
+        public List<PerfabPathInfo> leveledPerfabPaths;
+        private List<PerfabInfo> leveledPerfabs;
+
+        internal override GameObject GetGameObjects(uint level, Rot4 rot)
+        {
+            if(leveledPerfabs == null)
+            {
+                leveledPerfabs = new List<PerfabInfo>();
+                if(!leveledPerfabPaths.NullOrEmpty())
+                {
+                    leveledPerfabs.Capacity = leveledPerfabPaths.Count;
+                    foreach (PerfabPathInfo path in leveledPerfabPaths)
+                    {
+                        PerfabInfo info = new PerfabInfo();
+                        info.north = AssetBundleHelper.EffectAssetBundle.LoadAsset<GameObject>(path.north);
+                        info.south = AssetBundleHelper.EffectAssetBundle.LoadAsset<GameObject>(path.south);
+                        info.west = AssetBundleHelper.EffectAssetBundle.LoadAsset<GameObject>(path.west);
+                        info.east = AssetBundleHelper.EffectAssetBundle.LoadAsset<GameObject>(path.east);
+                        leveledPerfabs.Add(info);
+                    }
+                }
+            }
+            PerfabInfo targetLevel = leveledPerfabs[(int)level];
+            switch(rot.AsInt)
+            {
+                case Rot4.NorthInt:
+                return targetLevel.north;
+                case Rot4.SouthInt:
+                return targetLevel.south;
+                case Rot4.WestInt:
+                return targetLevel.west;
+                case Rot4.EastInt:
+                return targetLevel.east;
+                default:
+                return targetLevel.north;
+            }
+        }
+
+        public override void ResolveReferences(ThingDef parentDef)
+        {
+            parentDef.graphicData.graphicClass = typeof(Graphic_Perfab_Multi);
+            parentDef.graphic = null;
+            parentDef.drawerType = DrawerType.RealtimeOnly;
+        }
+
+        ~CompProperties_MultiPerfabDrawer()
+        {
+            foreach (PerfabInfo gameObject in leveledPerfabs)
+            {
+                if(gameObject.north) UnityEngine.Object.Destroy(gameObject.north);
+                if(gameObject.south) UnityEngine.Object.Destroy(gameObject.south);
+                if(gameObject.west) UnityEngine.Object.Destroy(gameObject.west);
+                if(gameObject.east) UnityEngine.Object.Destroy(gameObject.east);
+            }
+        }
     }
 }
