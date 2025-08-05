e9c7c1b0f8576d2345f22b9426c2e1e70303d870
MoonShine <ljhmyw@foxmail.com>
2025-08-06 00:10:56 +0800

    +测试版仓库

--- UpdateDLLRaw.bat
+++ UpdateDLLRaw.bat
@@ -2,8 +2,8 @@ DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\SoftRe
 COPY "C:\Users\Fleurety\Desktop\rw\SSR_Framework\Solar-System-Republic-Framework\FS_SSR\bin\Debug\FS_UGUIFramework.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\SoftRely\%1\AKFrameNotActive\Assemblies\"
 REN "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\SoftRely\AKFrameNotActive\%1\Assemblies\FS_UGUIFramework.dll" "AFS_UGUIFramework.dll"
 
-DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\Assemblies\%1\FS_SSR.dll"
-COPY "C:\Users\Fleurety\Desktop\rw\SSR_Framework\Solar-System-Republic-Framework\FS_SSR\bin\Debug\FS_SSR.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\%1\Assemblies\"
+DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\%1\Assemblies\FS_SSR.dll"
+COPY "C:\Users\Fleurety\Desktop\rw\SSR_Framework\Solar-System-Republic-Framework\FS_SSR\obj\DebugV16\FS_SSR.dll" "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\%1\Assemblies\"
 
 DEL "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\%1\Assemblies\FS_SSR_Combat.dll"
 COPY C:\Users\Fleurety\Desktop\rw\SSR_Framework\Solar-System-Republic-Framework\Combat\bin\Debug\FS_SSR_Combat.dll "S:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\SSR_Main\%1\Assemblies\"
